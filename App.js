6f11a14eb3ce7febf750282201473c6b70b51777
plaetzaw <plaetzaw@gmail.com>
2020-04-27 14:49:44 -0500

    add script start and / route for home

--- App.js
+++ App.js
@@ -2,12 +2,16 @@ const express = require("express");
 const app = express();
 const PORT = process.env.PORT || 8080;
 
-app.listen(PORT, () => {
-  console.log(`Listening on port ${PORT}`);
-});
-
 app.set("view engine", "ejs");
 app.use(express.static("public"));
 
 app.use(require("./routes/login"));
-app.use(require("./routes/createUser"))
+app.use(require("./routes/createUser"));
+
+app.get("/", (req, res) => {
+  res.status(500).json({ message: "HELLO WORLD!!!" });
+});
+
+app.listen(PORT, () => {
+  console.log(`Listening on port ${PORT}`);
+});
