e5461cd81559c1790f2ea2820518244496031e1d
plaetzaw <plaetzaw@gmail.com>
2020-04-27 15:43:05 -0500

    Firebase Set-Up Complete

--- App.js
+++ App.js
@@ -1,17 +1,7 @@
-const express = require("express");
-const app = express();
-const PORT = process.env.PORT || 8080;
-
-app.set("view engine", "ejs");
-app.use(express.static("public"));
-
-app.use(require("./routes/login"));
-app.use(require("./routes/createUser"));
-
-app.get("/", (req, res) => {
-  res.status(500).json({ message: "HELLO WORLD!!!" });
-});
-
-app.listen(PORT, () => {
-  console.log(`Listening on port ${PORT}`);
-});
+service firebase.storage {
+  match /b/{bucket}/o {
+    match /{allPaths=**} {
+      allow read, write: if request.auth!=null;
+    }
+  }
+}
