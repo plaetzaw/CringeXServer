643d364dc1a19674c75d60de3d55e88c00b8a4fc
plaetzaw <plaetzaw@gmail.com>
2020-04-30 13:29:55 -0500

    Testing Login and Register from Postman 200

--- App.js
+++ App.js
@@ -2,11 +2,17 @@ const express = require("express");
 const app = express();
 const PORT = process.env.PORT || 8080;
 
-app.listen(PORT, () => {
-  console.log(`Listening on port ${PORT}`);
-});
+// need to initialize and use body parser
+const bodyParser = require("body-parser");
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
 
 app.use(require("./routes/login"));
 app.use(require("./routes/createUser"));
 app.use(require("./routes/uploadPost"));
 app.use(require("./routes/contentFeed"));
+
+app.listen(PORT, () => {
+  console.log(`Listening on port ${PORT}`);
+});
--- routes/login.js
+++ routes/login.js
@@ -1,34 +1,39 @@
 const express = require("express");
 const router = express.Router();
-const bodyParser = require('body-parser');
-let db = require('../models');
-let bcrypt = require('bcrypt');
+const bodyParser = require("body-parser");
+let db = require("../models");
+let bcrypt = require("bcrypt");
 
 router.use(bodyParser.urlencoded({ extended: false }));
 
-router.post("/login", (req, res)=>{
-    let email = req.body.email;
-    let password = req.body.password;
+router.post("/login", (req, res) => {
+  let email = req.body.email;
+  let password = req.body.password;
 
-    db.user.findOne({
-        where: {
-            email : email
-        }
+  db.user
+    .findOne({
+      where: {
+        email: email,
+      },
     })
-    .then(currentUser =>{
-        if(currentUser){
-            bcrypt.compare(password, currentUser.password)
-            .then(correct => {
-                if(correct){
-                    console.log("user logged in")
-                } else {
-                    console.log("password is incorrect")
-                }
-            })
-        } else {
-            console.log("no user found")
-        }
-    })
-})
+    .then((currentUser) => {
+      if (currentUser) {
+        bcrypt.compare(password, currentUser.password).then((correct) => {
+          if (correct) {
+            console.log("user logged in");
+            // MAKE SURE YOU SEND A RESPONSE FROM THE SERVER - OTHERWISE YOUR CLIENT SIDE WILL KEEP SPINNING
+            res.status(200).json({ message: "USER LOGGED IN" });
+          } else {
+            console.log("password is incorrect");
+            // MAKE SURE YOU SEND A RESPONSE FROM THE SERVER - OTHERWISE YOUR CLIENT SIDE WILL KEEP SPINNING
+          }
+        });
+      } else {
+        console.log("no user found");
 
-module.exports = router
+        // MAKE SURE YOU SEND A RESPONSE FROM THE SERVER - OTHERWISE YOUR CLIENT SIDE WILL KEEP SPINNING
+      }
+    });
+});
+
+module.exports = router;
