0f15478b8092a93c2ca8b8db6a6534827476f86d
Matt Ryan <matthewtryan99@gmail.com>
2020-04-30 13:58:35 -0500

    jwt and contentFeed

--- routes/login.js
+++ routes/login.js
@@ -3,6 +3,7 @@ const router = express.Router();
 const bodyParser = require("body-parser");
 let db = require("../models");
 let bcrypt = require("bcrypt");
+const jwt = require('jsonwebtoken');
 
 router.use(bodyParser.urlencoded({ extended: false }));
 
@@ -20,9 +21,24 @@ router.post("/login", (req, res) => {
       if (currentUser) {
         bcrypt.compare(password, currentUser.password).then((correct) => {
           if (correct) {
+
+            const username = currentUser.handle
+            const id = currentUser.id
+            const email = currentUser.email
+
+            const user = { 
+                id: id,
+                name: username,
+                email: email
+            }
+        
+            const accessToken = jwt.sign(user, 'secret')
+        
+            res.json({accessToken : accessToken})
             console.log("user logged in");
             // MAKE SURE YOU SEND A RESPONSE FROM THE SERVER - OTHERWISE YOUR CLIENT SIDE WILL KEEP SPINNING
-            res.status(200).json({ message: "USER LOGGED IN" });
+
+            // res.status(200).json({ message: "USER LOGGED IN" });
           } else {
             console.log("password is incorrect");
             // MAKE SURE YOU SEND A RESPONSE FROM THE SERVER - OTHERWISE YOUR CLIENT SIDE WILL KEEP SPINNING
